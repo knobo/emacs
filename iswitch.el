7661472925a1f41d7fcc03f7055a5065bb12d08f
Knut Olav Bøhmer <bohmer@gmail.com>
2013-06-25 11:40:20 +0200

    * Improved iswitchb-kill-buffer-display to use iswitchb-modes own iswitchb-kill-buffer function * iswitchb-kill buffer renamed to iswitchb-kill-buffer-display

--- iswitch.el
+++ iswitch.el
@@ -1,23 +1,28 @@
 
-;; '(iswitchb-mode t)
+;; (iswitchb-mode)
 
 (defun iswitchb-cycle ()
   (interactive)
   (iswitchb-next-match)
   (display-buffer (first iswitchb-buflist) t))
 
-
-(defun iswitchb-kill-buffer ()
+(defun iswitchb-kill-buffer-display ()
   (interactive)
-  (kill-buffer (first iswitchb-buflist))
-  (setq iswitchb-buflist (rest iswitchb-buflist))
-  (display-buffer (first iswitchb-buflist) t)
+  (iswitchb-kill-buffer)
+  (display-buffer (second iswitchb-matches) t)
   (setq iswitchb-rescan t))
+
+(defun iswitchb-exclude-nonmatching()
+    "Make iswitchb work on only the currently matching names."
+    (interactive)
+    (setq iswitchb-buflist iswitchb-matches)
+    (setq iswitchb-rescan t)
+    (delete-minibuffer-contents))
 
 (eval-after-load 
     "iswitchb"
   '(add-hook 'iswitchb-define-mode-map-hook
              (lambda ()
                (define-key iswitchb-mode-map (kbd "M-c") 'iswitchb-cycle)
-               (define-key iswitchb-mode-map (kbd "M-q") 'iswitchb-kill-buffer))))
-               
+               (define-key iswitchb-mode-map (kbd "M-q") 'iswitchb-kill-buffer-display)
+	       (define-key iswitchb-mode-map (kbd "M-o") 'iswitchb-exclude-nonmatching))))
